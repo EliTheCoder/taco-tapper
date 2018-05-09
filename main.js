dea91ff5f12fc93e457ddd2da4ef68c9ca68928c
EliTheCoder <4386738+EliTheCoder@users.noreply.github.com>
2018-05-09 18:32:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -107,8 +107,11 @@ window.addEventListener("keydown", function(evt) {
   //9
   if (evt.keyCode == 57) {shop.buy(shop.solarsystems, 1, solarsystems);}
   // \
-  if (evt.keyCode == 220 && supertacoz === true) {supertacoz = false; alert("supertacoz off")}
-  elseif (evt.keyCode == 220 && supertacoz === false) {supertacoz = true; alert("supertacoz on")}
+  if (evt.keyCode == 220 && supertacoz === true) {
+    supertacoz = false; alert("supertacoz off");
+  } else {if (evt.keyCode == 220 && supertacoz === false) {
+    supertacoz = true; alert("supertacoz on")
+  }}
   // [
   if (evt.keyCode == 219 && supertacoz === true) {shop.tps = shop.tps * 2;}
 });
